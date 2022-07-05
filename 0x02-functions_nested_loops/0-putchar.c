c5f329f249fd36bcc565e6fee3dbc9773905f9a1
Collins Prosper <papilocollanso@gmail.com>
2022-07-04 23:17:52 -0930

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-putchar.c
+ 
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "COLLINS" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('C');
+	_putchar('O');
+	_putchar('L');
+	_putchar('L');
+	_putchar('I');
+	_putchar('N');
+	_putchar('S');
+
+	_putchar('\n');
+
+	return (0);
+}
